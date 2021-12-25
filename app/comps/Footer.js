a5586c79fd813da6b9ac86f3e7f752f275cb00ad
sudhakar <featuriz@gmail.com>
2021-12-25 20:55:22 +0530

    Added Navbar and Footer

---
+++ app/comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            Copyright 2021 My Ninja List
+        </div>
+    );
+}
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../app/comps/Footer'
+import Navbar from '../app/comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptatum hic consequatur. Et, laudantium repudiandae dolores commodi inventore repellat? Fugiat vero nulla ipsam! Laborum aut nulla deserunt assumenda hic neque.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptatum hic consequatur. Et, laudantium repudiandae dolores commodi inventore repellat? Fugiat vero nulla ipsam! Laborum aut nulla deserunt assumenda hic neque.</p>
+      <Footer />
     </div>
   )
 }
