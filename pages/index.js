1aad86681b8be296de15d3deb739e558e2417e1e
sudhakar <featuriz@gmail.com>
2021-12-25 21:01:46 +0530

    Link between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Footer from '../app/comps/Footer'
 import Navbar from '../app/comps/Navbar'
 import styles from '../styles/Home.module.css'
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>Homepage!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptatum hic consequatur. Et, laudantium repudiandae dolores commodi inventore repellat? Fugiat vero nulla ipsam! Laborum aut nulla deserunt assumenda hic neque.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptatum hic consequatur. Et, laudantium repudiandae dolores commodi inventore repellat? Fugiat vero nulla ipsam! Laborum aut nulla deserunt assumenda hic neque.</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
